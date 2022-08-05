86b618d5643a5c124827b19ff2a973584223b1fd
Татьяна Рыбакова <tatiana_rybakova@mail.ru>
2022-08-05 20:57:46 +0300

    BookShop

---
+++ OnlineShop/OnlineShop.Db/ProductsDbRepository.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShop.Db.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OnlineShop.Db
+{
+    public class ProductsDbRepository : IProductsRepository
+    {
+        private readonly DatabaseContext databaseContext;
+
+        public ProductsDbRepository(DatabaseContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }        
+        public List<Product> GetAll()
+        {
+            return databaseContext.Products.Include(x => x.Images).ToList();
+        }
+
+        public Product TryGetById(Guid id)
+        {
+            return databaseContext.Products.Include(x => x.Images).FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Add(Product product)
+        {
+            databaseContext.Products.Add(product);
+            databaseContext.SaveChanges();
+        }
+
+        public void Edit()
+        {            
+            databaseContext.SaveChanges();
+        }
+        public void Delete(Guid id)
+        {
+            var product = databaseContext.Products.FirstOrDefault(x => x.Id == id);
+            databaseContext.Products.Remove(product);
+            databaseContext.SaveChanges();
+        }
+
+        public List<Product> TryGetByName(string name)
+        {
+            //    List<Product> findProducts = new List<Product>();
+            //    {
+
+
+            return databaseContext.Products.Where(x => x.Name == name).Include(x => x.Images).ToList();
+                    
+                  
+
+            //    foreach (var product in databaseContext.Products)
+            //    {
+            //        if (product.Name.ToLower().Contains(name.ToLower()))
+            //        {
+            //            findProducts.Add(product.);
+            //        }
+            //    }
+            //}
+            //return findProducts;
+        }
+    }
+}
+
---
+++ OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Db;
+using OnlineShopWebApp.Helpers;
+using OnlineShopWebApp.Models;
+
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProductsRepository productsRepository;
+
+        public HomeController(IProductsRepository productsRepository)
+        {
+            this.productsRepository = productsRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var products = productsRepository.GetAll();
+            return View(Mapping.ToProductViewModels(products));
+        }
+
+        [HttpPost]
+        public IActionResult SearchByName(string searchname)
+        {
+            var findProduct = productsRepository.TryGetByName(searchname);
+            return View(Mapping.ToProductViewModels(findProduct));
+        }
+    }
+}
